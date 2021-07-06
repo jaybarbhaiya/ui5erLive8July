44f39b2f72ce482598d8ff868f874281e679f3aa
Jay Barbhaiya <jay.barbhaiya@sap.com>
2021-07-06 23:27:27 +0200

    initial state

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,5 +1,76 @@
-sap.ui.define(["com/myorg/myEasyUI5App/controller/BaseController"], function (Controller) {
+sap.ui.define([
+    "com/myorg/myEasyUI5App/controller/BaseController",
+    "sap/ui/model/json/JSONModel"
+], function (
+    Controller,
+    JSONModel
+) {
     "use strict";
 
-    return Controller.extend("com.myorg.myEasyUI5App.controller.MainView", {});
+    return Controller.extend("com.myorg.myEasyUI5App.controller.MainView", {
+        onInit: function() {
+            var aData = [{
+                "ProductName": "PC",
+                "ProductCode": "PD-101",
+                "Sold": "500",
+                "Produced": "750",
+                "Unit": "PCS",
+                "SoldPercent": 66.667,
+                "SoldPercentUnit": "%"
+            }, {
+                "ProductCode": "PD-102",
+                "ProductName": "Keyboard",
+                "Sold": "380",
+                "Produced": "400",
+                "Unit": "PCS",
+                "SoldPercent": 95,
+                "SoldPercentUnit": "%"
+            }, {
+                "ProductCode": "PD-103",
+                "ProductName": "Mouse",
+                "Sold": "320",
+                "Produced": "750",
+                "Unit": "PCS",
+                "SoldPercent": 42.667,
+                "SoldPercentUnit": "%"
+            }, {
+                "ProductCode": "PD-104",
+                "ProductName": "Chair",
+                "Sold": "200",
+                "Produced": "800",
+                "Unit": "PCS",
+                "SoldPercent": 25,
+                "SoldPercentUnit": "%"
+            }, {
+                "ProductCode": "PD-105",
+                "ProductName": "Table",
+                "Sold": "150",
+                "Produced": "550",
+                "Unit": "PCS",
+                "SoldPercent": 27.273,
+                "SoldPercentUnit": "%"
+            }, {
+                "ProductCode": "PD-106",
+                "ProductName": "Headsets",
+                "Sold": "850",
+                "Produced": "1000",
+                "Unit": "PCS",
+                "SoldPercent": 85,
+                "SoldPercentUnit": "%"
+            }, {
+                "ProductCode": "PD-107",
+                "ProductName": "Speakers",
+                "Sold": "997",
+                "Produced": "1200",
+                "Unit": "PCS",
+                "SoldPercent": 83.083,
+                "SoldPercentUnit": "%"
+            }];
+
+            var oModel = new JSONModel();
+            oModel.setData(aData);
+
+            this.getView().setModel(oModel);
+        }
+    });
 });
--- uimodule/webapp/model/formatter.js
+++ uimodule/webapp/model/formatter.js
@@ -1,4 +1,14 @@
 sap.ui.define([], function () {
     "use strict";
-    return {};
+    return {
+        soldPercentState: function(fSoldPercent) {
+            if (fSoldPercent <= 25) {
+                return "Error";
+            } else if (fSoldPercent > 25 && fSoldPercent <= 70) {
+                return "Warning";
+            } else {
+                return "Success";
+            }
+        }
+    };
 });
